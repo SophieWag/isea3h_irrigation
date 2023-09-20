315b8a89965e136f08eda1ee021b28a510b1878c
SophieWag <75021732+SophieWag@users.noreply.github.com>
2023-09-20 21:15:36 +0200

    Add files via upload

---
+++ Diff_Plotting.R
@@ -0,0 +1,64 @@
+####
+library(ggplot2)
+library(RColorBrewer)
+library(ggforce)
+library(pmr)
+
+fabianFol = "/Users/sophiewagner/Dropbox/HESS_Paper/Fabian/"
+
+data <- read.table(file=paste0(fabianFol,"diff_pred_ll.txt"),header = TRUE,sep =",")
+
+data <- data$diff_pred
+
+diff <- read.table(file=paste0(fabianFol,"diff_dgg.txt"),header = TRUE,sep =",")
+
+
+### Path
+load("/media/Storage/paper_plots_Sophie/Diff_Data.RData")
+resultFol = "/media/Storage/paper_plots_Sophie/"
+
+test_data$diff <- as.numeric(test_data$diff)
+
+range <- seq(-0.5,0.5,0.05)
+mycolors <- colorRampPalette(rev(brewer.pal(11, "Spectral")))(length(range)-1)
+
+png(paste0(fabianFol,"Diff_DGG.png"),width = 2000, height = 1000, units="px",res=400,pointsize = 4)
+ggplot(data = grid) +
+  geom_polygon(aes(x=long, y=lat, group=group, fill=diff)) +
+  scale_size_binned(breaks = range, guide = guide_bins(show.limits = T)) +
+  theme_void() +
+  binned_scale(aesthetics = "fill", scale_name = "custom", 
+               palette = ggplot2:::binned_pal(scales::manual_pal(values = mycolors)),
+               guide = "bins",
+               breaks = range, limits = c(-0.5, 0.5), show.limits = T)+
+  theme(legend.position = "none")
+
+dev.off()
+
+### Plot - LL Grid
+data = diff_pred
+brks=range
+palette=mycolors
+legendtitle = ""
+png(paste0(fabianFol,"Diff_LL.png"), width = 2000, height = 1000, units = "px", res = 400, pointsize = 4)
+if (!length(palette) == (length(brks) - 1)) {palette <- colorRampPalette(RColorBrewer::brewer.pal(9,palette))(length(brks) - 1)}
+ires <- 2 # inverse resolution (set to 2 for 0.5*0.5 degrees)
+legendticks <- seq(from = 0, to = 100, length.out = length(brks))
+data[data < brks[1]] <- brks[1]
+data[data > brks[length(brks)]] <- brks[length(brks)]
+ra <- raster::raster(ncols = 720, nrows = 360)
+range <- range(data)
+ra[raster::cellFromXY(ra,cbind(lon,lat))] <-  data
+extent <- raster::extent(c(-180, 180, -60, 90))
+par(bty = "n", oma = c(0,0,0,0), mar = c(0,0,0,3), xpd = T)
+raster::plot(ra, ext = extent, breaks = brks, col = palette, main = "",
+             legend = FALSE, axes = FALSE, maxpixels = 720*360)
+fields::image.plot(legend.only = TRUE, zlim = range(brks), col = palette, 
+                     useRaster = FALSE, breaks = legendticks,
+                     lab.breaks = round(brks,2), legend.shrink = 0.8, 
+                     legend.args = list("", side = 3, font = 2, line = 1))
+#maps::map('world', add = TRUE, res = 0, lwd = 0.1, ylim = c(-60,90))
+dev.off()
+
+
+  
